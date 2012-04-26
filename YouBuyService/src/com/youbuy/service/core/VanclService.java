9591d764dec7d434cef86417f8a999e7c5bfd59b
Han Pan <pandalovefei@gmail.com>
2012-04-26 20:33:25 +0800

    add more apis

--- YouBuyService/src/com/youbuy/service/core/VanclService.java
+++ YouBuyService/src/com/youbuy/service/core/VanclService.java
@@ -20,7 +20,7 @@ public class VanclService extends YoubuyService {
 	 * get the volume sales
 	 * 
 	 * @return string
-	 * @throws UnsupportedEncodingException 
+	 * @throws UnsupportedEncodingException
 	 */
 	public String getWeeklyranking() throws UnsupportedEncodingException {
 		url.append("/salesvolume/weeklyranking");
@@ -35,7 +35,7 @@ public class VanclService extends YoubuyService {
 	 * get males ranking
 	 * 
 	 * @return
-	 * @throws UnsupportedEncodingException 
+	 * @throws UnsupportedEncodingException
 	 */
 	public String getMalesRanking() throws UnsupportedEncodingException {
 		url.append("/salesvolume/malesranking");
@@ -50,7 +50,7 @@ public class VanclService extends YoubuyService {
 	 * get females ranking
 	 * 
 	 * @return
-	 * @throws UnsupportedEncodingException 
+	 * @throws UnsupportedEncodingException
 	 */
 	public String getLadiesRanking() throws UnsupportedEncodingException {
 		url.append("/salesvolume/ladiesranking");
@@ -63,21 +63,109 @@ public class VanclService extends YoubuyService {
 
 	/**
 	 * get the detailed information of product
-	 * @throws UnsupportedEncodingException 
+	 * 
+	 * @throws UnsupportedEncodingException
 	 */
-	public String getProductDetail(String productId) throws UnsupportedEncodingException {
+	public String getProductDetail(String productId)
+			throws UnsupportedEncodingException {
 		url.append("/product/detail");
-		vc.getMap().put("productcode", URLEncoder.encode(productId,"UTF-8"));
+		vc.getMap().put("productcode", URLEncoder.encode(productId, "UTF-8"));
 		vc.setSign();
 		String result = YoubuyService.handleGet(YoubuyService.handleArguments(
 				url, vc));
 		url = new StringBuffer(url.substring(0, 21));
 		return result;
 	}
+	
+	/**
+	 * get the detailed information of one color
+	 * @throws UnsupportedEncodingException 
+	 */
+	public String getProductDetailOfOneColor(String productId) throws UnsupportedEncodingException{
+		url.append("/product/productinfo");
+		vc.getMap().put("productcode", URLEncoder.encode(productId, "UTF-8"));
+		vc.setSign();
+		String result = YoubuyService.handleGet(YoubuyService.handleArguments(
+				url, vc));
+		url = new StringBuffer(url.substring(0, 21));
+		return result;
+	}
+	
+	/**
+	 * get all the products of the specific product's style
+	 * @throws UnsupportedEncodingException 
+	 */
+	public String getProductsForSample(String productId) throws UnsupportedEncodingException{
+		url.append("/product/producthome");
+		vc.getMap().put("productcode", URLEncoder.encode(productId, "UTF-8"));
+		vc.setSign();
+		String result = YoubuyService.handleGet(YoubuyService.handleArguments(
+				url, vc));
+		url = new StringBuffer(url.substring(0, 21));
+		return result;
+	}
+	
+	/**
+	 * get all the products of one styleId
+	 * @throws UnsupportedEncodingException 
+	 */
+	public String getProductsForStyle(String styleid) throws UnsupportedEncodingException{
+		url.append("/product/productlist");
+		vc.getMap().put("styleid", URLEncoder.encode(styleid, "UTF-8"));
+		vc.setSign();
+		String result = YoubuyService.handleGet(YoubuyService.handleArguments(
+				url, vc));
+		url = new StringBuffer(url.substring(0, 21));
+		return result;
+	}
+	
+	/**
+	 * get all the categories
+	 * @throws UnsupportedEncodingException 
+	 */
+	public String getAllCategories() throws UnsupportedEncodingException{
+		url.append("/category/list");
+		vc.setSign();
+		String result = YoubuyService.handleGet(YoubuyService.handleArguments(
+				url, vc));
+		url = new StringBuffer(url.substring(0, 21));
+		return result;
+	}
+	
+	/**
+	 * get comment list
+	 * @throws UnsupportedEncodingException 
+	 */
+	public String getCommentList(String styleid) throws UnsupportedEncodingException{
+		url.append("/product/commentlist");
+		vc.getMap().put("styleid", URLEncoder.encode(styleid, "UTF-8"));
+		vc.setSign();
+		String result = YoubuyService.handleGet(YoubuyService.handleArguments(
+				url, vc));
+		url = new StringBuffer(url.substring(0, 21));
+		return result;
+	}
+	
+	/**
+	 * get question list
+	 * @throws UnsupportedEncodingException 
+	 */
+	public String getQuestionList(String styleid) throws UnsupportedEncodingException{
+		url.append("/product/questionlist");
+		vc.getMap().put("styleid", URLEncoder.encode(styleid, "UTF-8"));
+		vc.setSign();
+		String result = YoubuyService.handleGet(YoubuyService.handleArguments(
+				url, vc));
+		url = new StringBuffer(url.substring(0, 21));
+		return result;
+	}
+	
+	
 
 	/**
 	 * get the hot searched keywords
-	 * @throws UnsupportedEncodingException 
+	 * 
+	 * @throws UnsupportedEncodingException
 	 */
 	public String getHotkeywords() throws UnsupportedEncodingException {
 		url.append("/search/hotkeywords");
@@ -97,7 +185,7 @@ public class VanclService extends YoubuyService {
 		VanclConfig vc = new VanclConfig();
 		vc.init();
 		VanclService vs = new VanclService(vc);
-		String result = vs.getHotkeywords();
+		String result = vs.getProductDetailOfOneColor("0051138");
 		System.out.println(result);
 		// DBParserUtil.parseDealVancl(result);
 	}
