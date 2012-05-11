aaba76415ba5882a8b705ffa4600832eb2d65b63
Han Pan <pandalovefei@gmail.com>
2012-05-11 20:48:40 +0800

    add get shopinfo api in 59 seconds service

--- YouBuyService/src/com/youbuy/service/core/FiftyNineSecondsService.java
+++ YouBuyService/src/com/youbuy/service/core/FiftyNineSecondsService.java
@@ -7,35 +7,78 @@ public class FiftyNineSecondsService extends YoubuyService {
 	/**
 	 * before any call should add the method interface and calculate the sign
 	 */
-	
+
 	private StringBuffer url;
 	private FiftyNineSecondConfig fnc;
-	
-	public FiftyNineSecondsService(FiftyNineSecondConfig fnc){
+
+	public FiftyNineSecondsService(FiftyNineSecondConfig fnc) {
 		this.fnc = fnc;
 		url = new StringBuffer("http://gw.api.59miao.com/Router/Rest");
 	}
-	
+
 	/**
-	 * to get the categories of commodity  
-	 * @throws UnsupportedEncodingException 
+	 * to get the categories of commodity
+	 * 
+	 * @throws UnsupportedEncodingException
 	 */
-	public String getItemCats() throws UnsupportedEncodingException{
+	public String getItemCats() throws UnsupportedEncodingException {
 		fnc.setMethod("59miao.itemcats.get");
 		fnc.getMap().put("parent_cid", 0);
 		fnc.getMap().put("fields", "cid,name");
 		fnc.setSign();
-		
-		String result = YoubuyService.handleGet(YoubuyService.handleArguments(url, fnc));
+
+		String result = YoubuyService.handleGet(YoubuyService.handleArguments(
+				url, fnc));
 		return result;
 	}
-	
-	public static void main(String[] args){
+
+	/**
+	 * to get the commodity list according to categories
+	 * 
+	 * @param category
+	 *            id
+	 * @throws UnsupportedEncodingException
+	 */
+	public String getItemListByCats(int cid, int pageNo)
+			throws UnsupportedEncodingException {
+		fnc.setMethod("59miao.items.search");
+		fnc.getMap().put("cid", cid);
+		fnc.getMap().put("page_size", 40);
+		fnc.getMap().put("page_no", pageNo);
+		fnc.getMap()
+				.put("fields",
+						"iid,click_url,seller_url,title,sid,seller_name,cid,desc,pic_url,price,cash_ondelivery,freeshipment,installment,has_invoice,modified,price_reduction,price_decreases,original_price");
+		fnc.setSign();
+		String result = YoubuyService.handleGet(YoubuyService.handleArguments(
+				url, fnc));
+		return result;
+	}
+
+	/**
+	 * to get the information of shop
+	 * 
+	 * @param sid
+	 * @throws UnsupportedEncodingException
+	 */
+	public String getShopInfo(int sid) throws UnsupportedEncodingException {
+		fnc.setMethod("59miao.shops.get");
+		fnc.getMap().put("sids", sid);
+		fnc.getMap()
+				.put("fields",
+						"sid,name,desc,shop_cat,logo,created,modified,click_url,cashback,payment,shipment,shipment_fee,cash_ondelivery,freeshipment,installment,has_invoice,status");
+		fnc.setSign();
+
+		String result = YoubuyService.handleGet(YoubuyService.handleArguments(
+				url, fnc));
+		return result;
+	}
+
+	public static void main(String[] args) {
 		FiftyNineSecondConfig fnc = new FiftyNineSecondConfig();
 		try {
 			fnc.init();
 			FiftyNineSecondsService fnss = new FiftyNineSecondsService(fnc);
-			System.out.println(fnss.getItemCats());
+			System.out.println(fnss.getShopInfo(1004));
 		} catch (UnsupportedEncodingException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
--- YouBuyService/src/com/youbuy/service/core/util/DBParserUtil.java
+++ YouBuyService/src/com/youbuy/service/core/util/DBParserUtil.java
@@ -92,6 +92,54 @@ public class DBParserUtil {
 			sr.close();
 		}
 	}
+	
+	
+	public static void parseDeal59Second(String dealXML) {
+		StringReader sr = new StringReader(dealXML);
+
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder;
+		
+		try {
+
+			dBuilder = dbFactory.newDocumentBuilder();
+
+			Document doc = dBuilder.parse(new InputSource(sr));
+			doc.getDocumentElement().normalize();
+
+			NodeList nl = doc.getElementsByTagName("");
+			int len = nl.getLength();
+			DealDAO dd = new DealDAO();
+			for (int i = 1; i < len-1; i++) {
+				Deal deal = new Deal();
+				Node node = nl.item(i);
+
+				if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element eElement = (Element) node;
+					String product_name = getTagValue("product_name", eElement);
+					String price = getTagValue("price", eElement);
+					String image_name = getTagValue("image_name", eElement);
+					String image_path = getTagValue("image_path", eElement);
+
+					deal.setDealDesc(product_name);
+					deal.setDealImg(image_path + image_name);
+					deal.setDealId(i);
+					System.out.println("dealid:" + i);
+					
+					dd.save(deal);
+				}
+			}
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			sr.close();
+		}
+	}
 
 	private static String getTagValue(String sTag, Element eElement) {
 		NodeList nlList = eElement.getElementsByTagName(sTag).item(0)
